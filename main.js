cf747bf60738b3ce75f5bc29b44f474a9742c88c
DaleWillemse <DaleWillemse000@gmail.com>
2022-06-06 22:33:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+
+//blink typing 
+const texts = ['Hi! My name is Dale Willemse.', 'Welcome to my portfolio.'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type(){
+    if(count === texts.length){
+        count = 0;
+    }
+    
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type,250);
+}());
+//blink typing end
+
+//Hamburger menu
+const navToggle = document.querySelector('.navToggle');
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+//Hamburger menu end
