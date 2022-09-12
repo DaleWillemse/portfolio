ec54f38bb15c6e34b29c809841c16db27344a2f0
DaleWillemse <DaleWillemse000@gmail.com>
2022-09-13 00:08:36 +0200

    trying to get the typing to pause on "."

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 
 //blink typing 
+
 const texts = ['Hi! My name is Dale.', 'Welcome to my portfolio.', 'Take a look =).'];
 let count = 0;
 let index = 0;
 let currentText = '';
 let letter = '';
 
-(function type(){
-    if(count === texts.length){
+(function type() {
+    if (count === texts.length) {
         count = 0;
     }
-    
+
     currentText = texts[count];
     letter = currentText.slice(0, ++index);
 
     document.querySelector('.typingText').textContent = letter;
-    if(letter.length === currentText.length){
+    if (letter.length === currentText.length) {
         count++;
         index = 0;
     }
-    setTimeout(type,250);
+    setTimeout(type, 200);
 }());
 //blink typing end
 
 //Hamburger menu
 const navToggle = document.querySelector('.navToggle');
-(function openNav(){
+(function openNav() {
     navToggle.addEventListener('click', () => {
         document.body.classList.toggle('nav-open');
     });
