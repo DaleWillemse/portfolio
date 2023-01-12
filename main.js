98f152b7116d7f882bc1e32b0a8a90c8ceb03a70
Dale Willemse <DaleWillemse000@gmail.com>
2023-01-12 21:05:17 +0200

    lets try this again.

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
-
-//blink typing 
-
-const texts = ['Hi! My name is Dale.', 'Welcome to my portfolio.', 'Take a look =).'];
+// Blink typing
+const texts = [
+  "Hi! My name is Dale.",
+  "Welcome to my portfolio.",
+  "Take a look :).",
+];
 let count = 0;
 let index = 0;
-let currentText = '';
-let letter = '';
+let currentText = "";
+let letter = "";
 
 (function type() {
-    if (count === texts.length) {
-        count = 0;
-    }
+  if (count === texts.length) {
+    count = 0;
+  }
 
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
 
-    document.querySelector('.typingText').textContent = letter;
-    if (letter.length === currentText.length) {
-        count++;
-        index = 0;
-    }
-    setTimeout(type, 200);
-}());
-//blink typing end
+  document.querySelector(".typingText").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 200);
+})();
+// Blink typing end
 
-//Hamburger menu
-const navToggle = document.querySelector('.navToggle');
+// Hamburger menu
+const navToggle = document.querySelector(".navToggle");
 (function openNav() {
-    navToggle.addEventListener('click', () => {
-        document.body.classList.toggle('nav-open');
-    });
-}());
-//Hamburger menu end
+  navToggle.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+  });
+})();
+// Hamburger menu end
+
+// Dark theme
+function darkTheme() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+// Dark theme end
